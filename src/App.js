3d1114155bf812bca19499d4f1e821d6f6afcd19
Fabian Fabro <fabrofabian@gmail.com>
2019-10-07 16:07:08 -0700

    implemented practice redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AllInstruments from './components/AllInstruments';
+import Counter from './container/counterContainer';
 
 class App extends React.Component {
   render(){
     return (
       <div className="App">
         <h1>Instruments</h1>
+        <Counter />
         <AllInstruments />
       </div>
     );
