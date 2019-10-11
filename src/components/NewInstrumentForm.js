944d8421a8863c6823cc09206846c28172794a97
Fabian Fabro <fabrofabian@gmail.com>
2019-10-10 17:22:51 -0700

    Can create new instrument

--- src/components/NewInstrumentForm.js
+++ src/components/NewInstrumentForm.js
@@ -1,19 +1,60 @@
 import React from 'react';
+import axios from 'axios';
 
 class NewInstrumentForm extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            name: "name",
+            origin: "origin",
+            year: 0,
+            classification: "classification"
+        }
+    }
+
+    handleChange = (e) => {
+        console.log(e.target.value);
+        this.setState({
+            [e.target.name]: e.target.value 
+        })
+    }
+
+    handleSubmit = (e) => {
+         e.preventDefault();
+
+         const API = 'http://localhost:52371/api/instruments/new';
+
+         axios.post(API, {
+             Name: this.state.name,
+             Origin: this.state.origin,
+             Year: this.state.year,
+             Classification: this.state.classification
+         })
+         .then(resp => {
+             console.log(resp.data)
+         })
+    }
+
+    // return(dispatch) => {
+    //     axios.get(API)
+    //     .then(resp => { 
+    //         dispatch(getInstruments(resp.data));
+    //     })
+    // }
+
     render(){
         return(
             <div className="main-container">
                 <h3>New Instrument Form</h3>
-                <form className="instrument-form">
+                <form className="instrument-form" onSubmit={this.handleSubmit}>
                     <label for="i-name">Instrument Name</label>
-                    <input type="text" name="name" placeholder="name"></input>
+                    <input type="text" name="name" placeholder="name" onChange={this.handleChange}></input>
                     <label for="i-origin">Origin</label>
-                    <input type="text" name="origin" placeholder="origin"></input>
+                    <input type="text" name="origin" placeholder="origin" onChange={this.handleChange}></input>
                     <label for="i-year">Year</label>
-                    <input type="number" name="year" placeholder="year"></input>
+                    <input type="number" name="year" placeholder="year" onChange={this.handleChange}></input>
                     <label for="i-class">Classification</label>
-                    <input type="text" name="classification" placeholder="classification"></input>
+                    <input type="text" name="classification" placeholder="classification" onChange={this.handleChange}></input>
                     <input type="submit" value="Submit"></input>
                     <input type="reset"></input>
                 </form>
