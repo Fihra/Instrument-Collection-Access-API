efe339bf25138dc203178e979d2cc7b62683140e
Fabian Fabro <fabrofabian@gmail.com>
2019-10-17 16:04:05 -0700

    added audience placeholder in canvas

--- src/components/Venue.js
+++ src/components/Venue.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Stage, Layer, Rect, Text, Tag, Label } from 'react-konva';
+import Konva from 'konva';
 
 class Venue extends React.Component {
+    state = {
+        color: Konva.Util.getRGB('#b3c5d7')
+    }
+
+    handleClick = () => {
+        this.setState({
+            color: Konva.Util.getRandomColor()
+        });
+    }
+
     render(){
+        let name = new Konva.Text({
+            x: 100,
+            y: 100,
+            text: 'Hello'
+        });
         return(
             <div>
+            <Stage width={900} height={700}>
+                <Layer>
+                                 
+                    <Rect x={0} y={550} width={900} height={500} fill={this.state.color}
+                     onClick={this.handleClick}
 
+                    />
+                    <Text text="Audience" x={200} y={600} fill="white"/>
+                </Layer>
+            </Stage>
             </div>
         )
     }
